fcc2910e6de25a090657b9e6e7ff62715a6fd40d
GuangCheng <sky9475@live.com>
2017-04-13 18:19:45 +0800

    Connection

--- client/chatsoftware/src/com/llj/network/SeverConnection.java
+++ client/chatsoftware/src/com/llj/network/SeverConnection.java
@@ -11,33 +11,32 @@ import org.jivesoftware.smack.parsing.ExceptionLoggingCallback;
 import org.jivesoftware.smack.tcp.XMPPTCPConnection;
 import org.jivesoftware.smack.tcp.XMPPTCPConnectionConfiguration;
 import org.jivesoftware.smack.util.TLSUtils;
+import org.pushingpixels.substance.api.shaper.RectangularButtonShaper;
 
 public class SeverConnection {
 	public static String severDNS = "ec2-54-254-130-230.ap-southeast-1.compute.amazonaws.com";
 	public static int severPort = 5222;
 	public static String xmppDomain = "ip-172-31-21-142.ap-southeast-1.compute.internal";
-	public static String userName = "lgc";
-	public static String userPass = "111111";
+	public static AbstractXMPPConnection connection;// Create a XMPPConnection
 
 	public static void login(String username, String pass) {
-
-		// Create a connection to the jabber.org server on a specific port.
+		// Create a connection to the server.
 		XMPPTCPConnectionConfiguration.Builder builder = XMPPTCPConnectionConfiguration.builder()
-				.setUsernameAndPassword(userName, userPass)
+				.setUsernameAndPassword(username, pass)
 				.setHost(severDNS)
 				.setPort(severPort)
 				.setServiceName(xmppDomain);
-
 		try {
-			TLSUtils.acceptAllCertificates(builder);// 信任所有证书
+			TLSUtils.acceptAllCertificates(builder);// Trust all certificates
 		} catch (NoSuchAlgorithmException | KeyManagementException e) {
 			e.printStackTrace();
 		}
 		try {
 			XMPPTCPConnectionConfiguration configuration = builder.build();
-			AbstractXMPPConnection connection = new XMPPTCPConnection(configuration);
+			connection = new XMPPTCPConnection(configuration);
 			connection.setParsingExceptionCallback(new ExceptionLoggingCallback());
 			connection.connect();
+			//return connection;
 		} catch (SmackException e1) {
 			// TODO Auto-generated catch block
 			e1.printStackTrace();
@@ -48,16 +47,16 @@ public class SeverConnection {
 			// TODO Auto-generated catch block
 			e1.printStackTrace();
 		}
-
 		try {
-			System.err.println(userName + ":" + userPass+" successfully login the xmpp server.");
+			System.err.println(username + ":" + pass+" successfully login the xmpp server.");
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		//return null;
 	}
 
 	public static void main(String[] args) {
-		login("111", "5555");
+		login("lgc", "111111");
 	}
 }
